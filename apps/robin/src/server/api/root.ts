8570f23e54fddcf83554ac5fef70e76730e51bfa
Raf <Rafcin.s@gmail.com>
2023-02-18 13:28:13 -0800

    More TRPC changes, test on Mac

--- apps/robin/src/server/api/root.ts
+++ apps/robin/src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from './trpc'
-import { exampleRouter } from './routers/example'
 import { mapsRouter } from './routers/maps'
 
 /**
@@ -8,7 +7,6 @@ import { mapsRouter } from './routers/maps'
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   maps: mapsRouter,
 })
 
